22ab655980a4b074d66fd275cd527069a6c055d6
e1rafax <101610037+e1rafax@users.noreply.github.com>
2024-03-11 15:47:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Define los elementos del girasol
-  var raiz = document.querySelector('.raiz');
-  var tallo = document.querySelector('.tallo');
-  var centro = document.querySelector('.centro');
-  var petalos = document.querySelectorAll('.petalo');
+  var petalElements = document.querySelectorAll('.petal');
 
-  // Oculta los elementos del girasol al principio
-  anime.set([raiz, tallo, centro, petalos], { scaleY: 0, scaleX: 0 });
-
-  // Define la secuencia de animaciones para el crecimiento y la floración
-  var girasolTimeline = anime.timeline({
-    easing: 'easeOutElastic',
-    duration: 2000
+  anime({
+    targets: petalElements,
+    rotate: '+=360deg',
+    easing: 'linear',
+    duration: 5000,
+    loop: true
   });
-
-  girasolTimeline
-    .add({
-      targets: raiz,
-      scaleY: 1,
-      scaleX: 1
-    })
-    .add({
-      targets: tallo,
-      scaleY: 1,
-      scaleX: 1
-    }, '-=1500')
-    .add({
-      targets: centro,
-      scaleY: 1,
-      scaleX: 1
-    }, '-=1000')
-    .add({
-      targets: petalos,
-      scaleY: 1,
-      scaleX: 1,
-      delay: anime.stagger(100)
-    }, '-=800');
 });
