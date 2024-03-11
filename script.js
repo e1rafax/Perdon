d00a16dbe800a1678bdda83aa55927c889af8620
e1rafax <101610037+e1rafax@users.noreply.github.com>
2024-03-11 16:16:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.querySelector('.sunflower').classList.add('grow');
-        setTimeout(function () {
-            document.querySelector('.flower').classList.add('bloom');
-        }, 1000);
-    }, 1000);
+document.addEventListener("DOMContentLoaded", function() {
+    var sunflower = document.querySelector('.sunflower');
+    var flower = document.querySelector('.flower');
+    var petals = document.querySelector('.petals');
+
+    // Espera un momento antes de iniciar la animación
+    setTimeout(function() {
+        sunflower.classList.add('grow');
+        setTimeout(function() {
+            flower.classList.add('bloom');
+            setTimeout(function() {
+                petals.classList.add('animate-petals');
+            }, 1000); // Espera antes de iniciar la animación de los pétalos
+        }, 2000); // Espera antes de hacer florecer la flor
+    }, 1000); // Espera antes de hacer crecer el girasol
 });
 
