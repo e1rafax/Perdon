71d573a47836b397d86acd92b4e850f1e2ca2a82
e1rafax <101610037+e1rafax@users.noreply.github.com>
2024-03-11 15:01:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,14 @@ function animateSunflower() {
   }, 500);
 
   // Creación de pétalos
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 10; i++) {
     const petal = document.createElement('div');
     petal.style.position = 'absolute';
-    petal.style.width = '60px';
-    petal.style.height = '30px';
+    petal.style.width = '100px';
+    petal.style.height = '50px';
     petal.style.backgroundColor = '#FFD700'; /* Color de los pétalos */
     petal.style.borderRadius = '50%';
-    petal.style.transform = `rotate(${i * 18}deg) translate(75px) rotate(-${i * 18}deg)`;
+    petal.style.transform = `rotate(${i * 36}deg) translate(100px) rotate(-${i * 36}deg)`;
     petals.appendChild(petal);
   }
 }
-
-
