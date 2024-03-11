01342b4c94a86f8fd5023d5504b3f7b9bae28816
e1rafax <101610037+e1rafax@users.noreply.github.com>
2024-03-11 16:08:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Animación de crecimiento y florecimiento del girasol
     setTimeout(function () {
         document.querySelector('.sunflower').classList.add('grow');
         setTimeout(function () {
             document.querySelector('.flower').classList.add('bloom');
-            setTimeout(function () {
-                document.querySelector('.petals').classList.add('animate-petals');
-            }, 1000);
-        }, 2000);
+        }, 1000);
     }, 1000);
 });
 
